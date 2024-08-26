ec6fa0384e48b78fd1077f0bcfda6e085667e58c
apperevezenceva <alla.perevezenceva@tk-alpha.ru>
2024-08-26 12:05:43 +0300

    [js] correct display header for scrolling

--- script.js
+++ script.js
@@ -2,17 +2,33 @@ document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("scroll", function () {
     let header = document.querySelector("header");
     let logo = document.querySelector(".logo");
+    let headerMiddle = document.querySelector(".vp-1024px");
     let scrollPosition = window.scrollY;
+    let innerWidth = window.innerWidth;
 
-    if ((window.innerWidth > 1024 && scrollPosition > 450) || (window.innerWidth <= 1024 && window.innerWidth > 360 && scrollPosition > 100) || (window.innerWidth <= 360 && scrollPosition > 50) ) {
+    let scrollThreshold = 450; 
+
+    if (innerWidth <= 1024) {
+      scrollThreshold = 350;
+    } 
+    if (innerWidth <= 360) {
+      scrollThreshold = 50; 
+    }
+
+    if (scrollPosition > scrollThreshold) {
       header.classList.remove("header__ordinary");
       logo.src = "./assets/YourTour-dark.png";
       header.classList.add("header__fix");
+      if (innerWidth <= 1024 ) {
+        headerMiddle.style.display = "block";
+      } 
     } else {
       header.classList.remove("header__fix");
       logo.src = "./assets/YourTour.png";
       header.classList.add("header__ordinary");
+      headerMiddle.style.display = "none";
     }
+
   });
 
   let direction = document.querySelector("#direction");
