57d9fc9663115895375b21f5a89b575127d9def6
apperevezenceva <alla.perevezenceva@tk-alpha.ru>
2024-08-26 11:37:44 +0300

    [js] add viewports for scrolling

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let logo = document.querySelector(".logo");
     let scrollPosition = window.scrollY;
 
-    if (scrollPosition > 450) {
+    if ((window.innerWidth > 1024 && scrollPosition > 450) || (window.innerWidth <= 1024 && window.innerWidth > 360 && scrollPosition > 100) || (window.innerWidth <= 360 && scrollPosition > 50) ) {
       header.classList.remove("header__ordinary");
       logo.src = "./assets/YourTour-dark.png";
       header.classList.add("header__fix");
@@ -15,12 +15,11 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  let direction = document.querySelector('#direction');
-  direction.addEventListener('change', function() {
-      const selectedValue = this.value;
-      if (selectedValue !== "0") {
-          this.classList.toggle('active-variant');
-      } 
+  let direction = document.querySelector("#direction");
+  direction.addEventListener("change", function () {
+    const selectedValue = this.value;
+    if (selectedValue !== "0") {
+      this.classList.toggle("active-variant");
+    }
   });
-  
 });
